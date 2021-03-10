d21cbec2e9ee4a029fba8ab8584b80be11c00198
Tom <tadams@and.digital>
2021-03-10 13:24:32 +0000

    Changes

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -2,9 +2,28 @@ import { useQuery } from "@apollo/client";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import { Document } from "@contentful/rich-text-types";
 import React from "react";
+import styled from "styled-components";
 import { GetAbout } from "../../helpers/queries";
 import Container from "../Shared/container";
 import Loading from "../Shared/loading";
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const StyledText = styled.p`
+  text-align: left;
+  max-width: 50%;
+  align-items: flex-start;
+  margin-left: 20px;
+  margin-right: 100px;
+`;
+const StyledImg = styled.img`
+  text-align: right;
+  align-items: flex-end;
+  width: 500px;
+  height: 500px;
+`;
 
 function About() {
   const { data, loading } = useQuery(GetAbout);
@@ -15,8 +34,14 @@ function About() {
         <Loading />
       ) : (
         data.aboutCollection.items.map(
-          (document: { text: { json: Document } }) =>
-            documentToReactComponents(document.text.json)
+          (document: { text: { json: Document }; image: { url: string } }) => (
+            <Wrapper>
+              <StyledText>
+                {documentToReactComponents(document.text.json)}
+              </StyledText>
+              <StyledImg src={document.image.url} />
+            </Wrapper>
+          )
         )
       )}
     </Container>
